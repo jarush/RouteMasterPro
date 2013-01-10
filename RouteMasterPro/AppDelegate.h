937acd73e1b8e1ec54842e1eec5e1b680852b5f8
Jason Rush <jason@fizzawizza.com>
2013-01-10 08:34:55 -0600

    Initial Commit

---
+++ RouteMasterPro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RouteMasterPro
+//
+//  Created by Jason Rush on 1/10/13.
+//  Copyright (c) 2013 Flush Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RouteMasterPro/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RouteMasterPro
+//
+//  Created by Jason Rush on 1/10/13.
+//  Copyright (c) 2013 Flush Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
