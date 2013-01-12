927829b2926730ac0b688ce1f265c8014b12733c
Jason Rush <jason@fizzawizza.com>
2013-01-12 13:27:08 -0600

    Add logic to display route files

--- RouteMasterPro/RoutesViewController.m
+++ RouteMasterPro/RoutesViewController.m
@@ -8,8 +8,9 @@
 
 #import "RoutesViewController.h"
 
-@interface RoutesViewController ()
-
+@interface RoutesViewController () {
+    NSArray *_files;
+}
 @end
 
 @implementation RoutesViewController
@@ -22,24 +23,46 @@
     return self;
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (void)dealloc {
+    [_files release];
+    [super dealloc];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsPath = [paths objectAtIndex:0];
+    NSArray *contentsOfDirectory = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:documentsPath error:nil];
+
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"self endswith[c] '.xml'"];
+    _files = [[contentsOfDirectory filteredArrayUsingPredicate:predicate] retain];
+
+    [self.tableView reloadData];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+    return [_files count];
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return nil;
+    static NSString *CellIdentifier = @"Cell";
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    }
+
+    cell.textLabel.text = [_files objectAtIndex:indexPath.row];
+
+    return cell;
 }
 
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-
 }
 
 @end
